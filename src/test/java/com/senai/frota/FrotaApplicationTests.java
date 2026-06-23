64481993e7df98520a3f7e02cae5e78f32cbb4ad
F-TNK <fabiotanakasantos@gmail.com>
2026-06-23 11:37:47 -0300

    first

---
+++ src/test/java/com/senai/frota/FrotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.frota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
