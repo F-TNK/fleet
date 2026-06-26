f5fecf8af6f5c1fca251d8a4560330b9e54b8e25
F-TNK <fabiotanakasantos@gmail.com>
2026-06-26 17:24:52 -0300

    begin front

--- src/main/java/com/senai/frota/controller/UserController.java
+++ src/main/java/com/senai/frota/controller/UserController.java
@@ -7,13 +7,18 @@ package com.senai.frota.controller;
 import com.senai.frota.model.UserDTO;
 import com.senai.frota.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Micro
  */
+@RestController
+@RequestMapping("/api/autenticar")
 public class UserController {
     
     @Autowired
@@ -23,6 +28,11 @@ public class UserController {
     public String registrar(@RequestBody UserDTO u) {
         uservice.register(u);
         return "Cadastro realizado com sucesso!";
+    }
+    
+    @GetMapping("/login")
+    public String login(@RequestBody UserDTO u){
+        return uservice.login(u); 
     }
     
 }
--- src/main/java/com/senai/frota/repository/LiberacaoDAO.java
+++ src/main/java/com/senai/frota/repository/LiberacaoDAO.java
@@ -21,71 +21,71 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class LiberacaoDAO {
     
-    public List<LiberacaoDTO> listaLiberacoes(){
-        List<LiberacaoDTO> lista = new ArrayList<>();
-        
-        try {
-            
-            Connection conn = Conexao.connect();
-            PreparedStatement stmt = conn.prepareStatement("Select * from liberacoes");
-            ResultSet rs = stmt.executeQuery();
-            
-            while (rs.next()) {
-                LiberacaoDTO l = new LiberacaoDTO();
-
-                l.setId(rs.getLong("idLiberacao"));
-                l.setIdOperador(rs.getLong("operador"));
-                l.setIdEquip(rs.getLong("equip"));
-                l.setDataHoraRetirada(rs.getLocalDateTime("data_hora_retirada"));
-                l.setDataHoraDevolucao(rs.getLocalDateTime("data_hora_devolucao"));
-                l.setHorimetroInicial(rs.getDouble("horimetro_inicial"));
-                l.setHorimetroFinal(rs.getDouble("horimetro_final"));
-                l.setCombustivelInicial(rs.getDouble("combustivel_inicial"));
-                l.setCombustivelFinal(rs.getDouble("combustivel_final"));
-                l.setLocalUso(rs.getString("local_uso"));
-                l.setObservacoes(rs.getString("observacoes"));
-
-                lista.add(l);
-            }
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-        return lista;
-    }
-    
-    
-    public int novaLiberacao(LiberacaoDTO lib){
-        
-        try {
-            
-            Connection conn = Conexao.connect();
-            PreparedStatement stmt = conn.prepareStatement(
-                    "insert into liberacoes "
-                    + "(operador, equipamento, data_hora_retirada, data_hora_devolucao, horimetro_inicial, "
-                    + "horimetro_final, combustivel_inicial, combustivel_final, local_uso, observacoes) "
-                    + "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-            stmt.setLong(1, lib.getIdOperador());
-            stmt.setLong(2, lib.getIdEquip());
-            stmt.setString(3, lib.getDataHoraRetirada());
-            stmt.setString(4, lib.getDataHoraDevolucao());
-            stmt.setDouble(5, lib.getHorimetroInicial());
-            stmt.setDouble(6, lib.getHorimetroFinal());
-            stmt.setDouble(7, lib.getCombustivelInicial());
-            stmt.setDouble(8, lib.getCombustivelFinal());
-            stmt.setString(9, lib.getLocalUso());
-            stmt.setString(10, lib.getObservacoes());
-
-            return stmt.executeUpdate();
-            
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-        
-        return 0;
-    }
+//    public List<LiberacaoDTO> listaLiberacoes(){
+//        List<LiberacaoDTO> lista = new ArrayList<>();
+//        
+//        try {
+//            
+//            Connection conn = Conexao.connect();
+//            PreparedStatement stmt = conn.prepareStatement("Select * from liberacoes");
+//            ResultSet rs = stmt.executeQuery();
+//            
+//            while (rs.next()) {
+//                LiberacaoDTO l = new LiberacaoDTO();
+//
+//                l.setId(rs.getLong("idLiberacao"));
+//                l.setIdOperador(rs.getLong("operador"));
+//                l.setIdEquip(rs.getLong("equip"));
+//                l.setDataHoraRetirada(rs.getLocalDateTime("data_hora_retirada"));
+//                l.setDataHoraDevolucao(rs.getLocalDateTime("data_hora_devolucao"));
+//                l.setHorimetroInicial(rs.getDouble("horimetro_inicial"));
+//                l.setHorimetroFinal(rs.getDouble("horimetro_final"));
+//                l.setCombustivelInicial(rs.getDouble("combustivel_inicial"));
+//                l.setCombustivelFinal(rs.getDouble("combustivel_final"));
+//                l.setLocalUso(rs.getString("local_uso"));
+//                l.setObservacoes(rs.getString("observacoes"));
+//
+//                lista.add(l);
+//            }
+//            
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        
+//        return lista;
+//    }
+//    
+//    
+//    public int novaLiberacao(LiberacaoDTO lib){
+//        
+//        try {
+//            
+//            Connection conn = Conexao.connect();
+//            PreparedStatement stmt = conn.prepareStatement(
+//                    "insert into liberacoes "
+//                    + "(operador, equipamento, data_hora_retirada, data_hora_devolucao, horimetro_inicial, "
+//                    + "horimetro_final, combustivel_inicial, combustivel_final, local_uso, observacoes) "
+//                    + "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+//
+//            stmt.setLong(1, lib.getIdOperador());
+//            stmt.setLong(2, lib.getIdEquip());
+//            stmt.setDate(3, lib.getDataHoraRetirada());
+//            stmt.setDate(4, lib.getDataHoraDevolucao());
+//            stmt.setDouble(5, lib.getHorimetroInicial());
+//            stmt.setDouble(6, lib.getHorimetroFinal());
+//            stmt.setDouble(7, lib.getCombustivelInicial());
+//            stmt.setDouble(8, lib.getCombustivelFinal());
+//            stmt.setString(9, lib.getLocalUso());
+//            stmt.setString(10, lib.getObservacoes());
+//
+//            return stmt.executeUpdate();
+//            
+//        } catch (SQLException e){
+//            e.printStackTrace();
+//        }
+//        
+//        return 0;
+//    }
     
     
     
